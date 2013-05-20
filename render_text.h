d24254b53d30d9b77d3d61d643519979a2170fed
Mark Tozzi <mark.tozzi@gmail.com>
2013-05-19 23:01:41 -0400

    Missed a now-private function in render_text

--- render_text.h
+++ render_text.h
@@ -5,6 +5,7 @@
 
 #include <graphics_wrapper.h>
 
+/* TODO: Rename this to like RenderedWordList or some shit */
 typedef struct SurfaceNode *SurfaceNodePtr;
 
 typedef enum {
@@ -12,7 +13,6 @@ typedef enum {
   scroll_down = 1
 } ScrollDir;
 
-void free_surface_list(SurfaceNodePtr slist);
 int build_word_list(const char *text, SurfaceNodePtr *dest, Color color,
     TTF_Font *font);
 int get_message_height(SurfaceNodePtr msg, int w);
