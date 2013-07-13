d2da84ec989de91367376ad9b1027ef9e9bce99d
Mark Tozzi <mark.tozzi@gmail.com>
2013-07-13 03:53:04 -0400

    Added reference link for seed algorithm

--- seed_fill.h
+++ seed_fill.h
@@ -4,6 +4,12 @@
 #include <map.h>
 #include <tile.h>
 
+/*
+ * Fill space using a cellular automata "seed" model.  See:
+ * https://code.google.com/p/jetblade/wiki/MapGeneration
+ * for algorithm description
+ */
+
 void splat(MapSection *map, const int matrix_x, const int matrix_y,
     const TileTypeIndex *terrain_walls, const int terrain_count);
 #endif
