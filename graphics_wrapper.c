032e04b023a80162231510575af35077d7ed0fbb
Mark Tozzi <mark.tozzi@gmail.com>
2014-04-27 11:08:37 -0400

    Fix clear_rect, nuke draw_rect

--- graphics_wrapper.c
+++ graphics_wrapper.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
 
 #include "SDL.h"
+#include "SDL_ttf.h"
 
 #include <graphics_wrapper.h>
 
 #define LOGGING_ENABLED
 #include <log.h>
 
-static SDL_Window *screen == NULL;
-static SDL_Renderer *main_renderer == NULL;
+static SDL_Window *screen = NULL;
+static SDL_Renderer *main_renderer = NULL;
 
 SDL_Window *get_screen() {
   if (screen == NULL) {
@@ -45,10 +46,8 @@ void init_graphics(int window_w, int window_h) {
 }
 
 void clear_rect(const Rect *r) {
-  SDL_Rect tmp = {r->x, r->y, r->w, r->h};
-  SDL_Surface *screen;
-  screen = get_screen();
-  int ret = SDL_FillRect(screen, &tmp, SDL_MapRGB(screen->format, 0, 0, 0));
+  SDL_SetRenderDrawColor(get_main_renderer(), 0, 0, 0, SDL_ALPHA_OPAQUE);
+  int ret = SDL_RenderDrawRect(get_main_renderer(), r);
   if (ret != 0) {
     CRITICAL("SDL_FillRect failed with exit code %d for rect "
        "(x:%d, y: %d, w:%d, h:%d)\n", ret, r->x, r->y, r->w, r->h);
@@ -66,12 +65,4 @@ Rect make_rect(Sint16 x, Sint16 y, Uint16 w, Uint16 h) {
   ret.w = w;
   ret.h = h;
   return ret;
-}
-
-void draw_rect(const Rect *r) {
-  TRACE("Drawing rect x: %d, y: %d, w: %d, h: %d\n", r->x, r->y, r->w, r->h);
-  SDL_Surface *screen;
-  screen = get_screen();
-  SDL_UpdateRect(screen, r->x, r->y, r->w, r->h);
-  TRACE("Screen updated\n");
 }
