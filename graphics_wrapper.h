755f5556975c08a91c8f119e3f9fb1093c114531
Mark Tozzi <mark.tozzi@gmail.com>
2014-05-03 08:53:43 -0400

    Cleanup and notes on graphics_wrapper

--- graphics_wrapper.h
+++ graphics_wrapper.h
@@ -2,6 +2,9 @@
 #define GRAPHICS_WRAPPER_H_INCLUDED
 
 #include <stdint.h>
+
+/* TODO: Kill get_screen and get_main_renderer so I can get SDL out of this
+ * header. */
 #include "SDL.h"
 
 typedef struct Rect {
@@ -19,7 +22,6 @@ SDL_Renderer *get_main_renderer();
 SDL_Window *get_screen();
 void clear();
 Rect make_rect(int16_t x, int16_t y, uint16_t w, uint16_t h);
-void free_assets();
 void flip();
 
 #endif
